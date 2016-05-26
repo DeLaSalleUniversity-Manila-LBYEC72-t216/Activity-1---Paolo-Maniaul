898ca897d8c285bd55a80ba50a5c340d1822df12
Pao <paolo_maniaul@dlsu.edu.ph>
2016-05-26 16:44:49 +0800

    Create ACTIVITY1C.c

---
+++ ACTIVITY1C.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	
+	float x,func;
+	printf("Enter value of X: ");
+	scanf("%f", &x);
+	func=x/70;
+	printf("\nf(x)=%f\n\n", func);
+	
+	system("PAUSE");
+	return 0;
+	
+}
